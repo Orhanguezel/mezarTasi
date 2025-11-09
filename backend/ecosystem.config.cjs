5eca309431072fb101b787d13e7ff88184955ee2
Orhan Güzel <orhanguzell@gmail.com>
2025-11-09 23:40:02 +0100

    alles super

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       max_memory_restart: "300M",
       env: {
         NODE_ENV: "production",
-        PORT: "8082"                     // Nginx'te /api -> 8082 ise burayı 8082 yap
+        PORT: "8083"                     // Nginx'te /api -> 8083 ise burayı 8083 yap
       },
       out_file: "/var/log/pm2/mezartasi-backend.out.log",
       error_file: "/var/log/pm2/mezartasi-backend.err.log",
